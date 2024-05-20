27a3ef316017bc53f2f88790ec7dd20dcddfec4c
Stelak79 <stelak@abv.bg>
2024-05-20 21:19:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//сума = депозирана сума + срок на депозита * ((депозирана сума * годишен лихвен процент ) / 12)
+
+
+double deposit = double.Parse(Console.ReadLine());
+int term = int.Parse(Console.ReadLine());
+double rate = double.Parse(Console.ReadLine()) / 100;
+double sum = deposit + term * (deposit * rate / 12);
+Console.WriteLine(sum);
